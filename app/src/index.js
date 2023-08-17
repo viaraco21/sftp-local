6344dd7f50348fdbdcc31d8e61585c037e7e89c3
Renato Aurelio <viaraco21@gmail.com.br>
2023-08-17 14:49:58 -0300

    atualizando o projeto

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const users = require('./lib/app');
 const app = express();
+const log = (req, res, next) => {
+  console.log(`${req.method} ${req.originalUrl}`);
+  next();
+}
 
-app.get('/users/:topology', users.get);
-app.post('/users/:topology', users.post);
-app.delete('/users/:topology', users.deleteUser);
+app.get('/users/',log, users.listartodos);
+app.get('/users/:usuario',log, users.buscarusuario); ///////////////
+app.post('/users/:usuario',log, users.atualizausuario);
+app.put('/users/:usuario',log, users.incluiusuario);
+app.delete('/users/:usuario',log, users.deletausuario);
 
 const port = process.env.PORT || 4000;
 
--- app/src/lib/app.js
+++ app/src/lib/app.js
@@ -1,3 +1,136 @@
+'use strict'
+
+const USERSCONF = "users.conf";
+
+const fs = require('fs')
+
+const listartodos = async (req, res) => {
+  let retorno = true
+
+  try {
+    const data = fs.readFileSync(USERSCONF, 'utf8')
+    return res.json(data);
+
+  } catch (err) {
+
+    return res.status(404).json({ error: 'Sem usuario' });
+  }
+}
+
+///////////////////////////////////////////////////////
+
+const buscarusuario = async (req, res) => {
+  let usuario = req.params.usuario;
+  const data = fs.writeFileSync(USERSCONF, usuario = "", 'utf8')
+  try {    
+     if ((usuario == codigo)){ 
+        console.log("Usuário já cadastrado!");
+        return res.json(data);} 
+
+     else ((usuario != codigo)){ 
+        return res.status(404).json({ error: 'Usuário não cadastrado!' });
+      }
+  }
+}
+
+////////////////////////////////////////////////////////
+
+const atualizausuario = async (req, res) => {
+  let usuario = req.params.usuario;
+  let { password } = req.body;
+
+  return;
+};
+
+const incluiusuario = async (req, res) => {
+  let { usuario, password } = req.body;
+
+  return;
+};
+
+const deletausuario = async (req, res) => {
+  let usuario = req.params.usuario;
+
+  return;
+};
+
+
+module.exports = { listartodos, buscarusuario, atualizausuario, incluiusuario, deletausuario };
+
+/*
+
+module.exports = {
+    create: function () {
+        if (!fs.existsSync(USERSCONF)) {
+            grava("admin:admin:::admin");
+        };
+    },
+
+    listarUsers: function () {
+        try {
+            const data = fs.readFileSync(USERSCONF, 'utf8')
+            console.log(data)
+            return data;
+        } catch (err) {
+            console.error(err)
+            return
+        }
+
+    },
+
+    : function (user) {
+        let oUsers = listarUsers();
+
+        oUsers.filter(u)
+    },
+
+    incluirUser: function (user, password) {
+buscarUser
+        buscarUser(user).then((u) => {
+            console.log("Usuário já cadastrado!");
+            return
+        })
+
+        let content = user + ":" + password + ":::" + user
+
+        grava(content).then(r => {
+            console.log("Usuario cadastrado com sucesso!");
+            return
+        })
+          
+    },
+    alterarUser: function (user, password) {
+        oUsers = buscarUser(user);
+        oUsers.user = user;
+        oUsers.password = password;
+        oUsers.grava();
+
+    },
+    excluirUser: function (user) {
+        oUsers = buscarUser(user);
+        oUsers.user = user;
+        oUsers.delete(); 
+    }
+}
+
+function grava(content) {
+    try {
+        const data = fs.writeFileSync(USERSCONF, content)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+const USERSCONF = "users.conf";
 const fs = require('fs');
 const BINARY_FILE = "/usr/local/bin/script.sh";
 
@@ -18,7 +151,7 @@ const deleteUsersConf = async (user) => {
   return retorno;
 };
 
-const get = async (req, res) => {
+******const get = async (req, res) => {
   const {
     query: { user }
   } = req;
@@ -70,4 +203,39 @@ const deleteUser = async (req, res) => {
   res.send({ message: 'Usuário Deletado' });
 };
 
-module.exports = { get, post, deleteUser };
+module.exports = { listartodos,buscarusuario,atualizausuario,incluiusuario,deletausuario };
+
+//criei um objeto chamado rotas
+const rotas = {
+  '/ listartodos': ($USERSCONF) 
+}
+
+const buscarusuario = async (req, res) => {
+  let usuario = req.params.usuario;
+  try {
+    const data = fs.readFileSync(usuario, 'utf8')
+    return res.json(data); 
+    
+    } catch (err) {
+  return res.status(404).json({error: 'Usuario não encontrado'}); 
+}
+}
+
+   let content = usuario + ":" + password + ":" + codigo
+
+      grava(content).then(r => {
+        console.log("Usuario cadastrado com sucesso!");
+        return
+      })
+    }
+  }
+
+  function (usuario, password, codigo) {
+      buscarusuario(usuario).then((codigo) => {
+        console.log("Usuário já cadastrado!");
+        return
+      })
+      console.log("Usuário não cadastrado!");
+}
+
+*/
